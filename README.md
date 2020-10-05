8d46e0c860c77dd66c62c49baf7e525da279a2a1
schurterb <brandon.schurter@protonmail.com>
2020-10-04 22:14:29 -0400

    Added the option to search for the best number of tables that can be arranged.  Updated README.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # table-optimizer
 A genetic algorithm for the optimization of socially distant table locations during the Covid-19 pandemic.
+
+This optimizer can be run from a terminal or command prompt with the following command:
+```
+python3 table-optimizer.py [FLAGS]
+```
+It accepts one of two flags:
+
+* --optimize-arrangement  Optimizes the positioning of tables within a room, 
+                            as defined in the config file.
+* --optimize-number       Optimizes the number of tables within the room,
+                            as well as their positioning.
+                            
+### Configuration
+
+The table optimizer algorithm can be configured via the config.ini.  The purpose
+of each of the parameters in the config file is listed below.  For users not 
+familiar with evolutionary or genetic algorithms, it is recommened to leave the
+'[algorithm]' section as is.
+
+The '[room]' section defines the size and properties of the room in which
+the tables will be positioned.
+```
+[room]             
+minimum_separation // The minimum allowed distance between tables (in feet)
+wall_margin = 6    // The minimum distance between a table and the wall (in feet)
+room_length = 83   // The length of the room (in feet)
+room_width = 60    // The width of the room (in feet)
+```
+
+The '[tables]' section defines the tables that should be placed in the room.
+Currently, only round and square tables are supported.
+```
+[tables]
+number_of_round_tables    // The number of circular tables
+round_table_radius        // The radius of each circular table
+number_of_square_tables   // The number of square tables
+square_table_side_length  // The length of one side of each square table
+```
+
+The '[algorithm]' configures the algorithm itself.
+```
+[algorithm]               
+number_of_arrangements   // The population of each generation
+number_of_generations
+percent_parents_from_previous_generation = 0.01
+percent_new_arrangements_each_generation = 0.50
+```
--- table-optimizer.py
+++ table-optimizer.py
@@ -1,4 +1,4 @@
-#!/home/user/anaconda3/bin/python3
+#!/usr/bin/python3
 
 """
 Created on Sat Sep 26 20:32:33 2020
@@ -11,27 +11,34 @@ from random import Random
 from enum import Enum
 import matplotlib.pyplot as plt
 from math import sqrt
+import configparser
+import argparse
 
 ##############################################################################
-def main():    
-    min_distance=10
-    x_boundaries=[0,99]
-    y_boundaries=[0,71]
-    
-    num_round_tables=15
-    round_table_radius=3
-    num_square_tables=3
-    square_table_radius=2
-    
-    num_arrangements=200
-    num_generations=300
-    percent_parents_from_previous_generation=0.01
-    percent_new_arrangements_each_generation=0.80
-        
+def optimize_arrangement():
     print("Begin Table Optimization")
     
+    print("Loading configeration parameters")
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    min_distance=float(config['room']['minimum_separation'])
+    wall_margin=float(config['room']['wall_margin'])
+    x_boundaries=[0,int(config['room']['room_length'])]
+    y_boundaries=[0,int(config['room']['room_width'])]
+    
+    num_round_tables=int(config['tables']['number_of_round_tables'])
+    round_table_radius=float(config['tables']['round_table_radius'])
+    num_square_tables=int(config['tables']['number_of_square_tables'])
+    square_table_radius=float(config['tables']['square_table_side_length'])/2.0
+    
+    num_arrangements=int(config['algorithm']['number_of_arrangements'])
+    num_generations=int(config['algorithm']['number_of_generations'])
+    percent_parents_from_previous_generation=float(config['algorithm']['percent_parents_from_previous_generation'])
+    percent_new_arrangements_each_generation=float(config['algorithm']['percent_new_arrangements_each_generation'])
+    
+    
     print("Initializing Tables")
-    a = arrangement(min_distance, x_boundaries, y_boundaries)
+    a = arrangement(x_boundaries, y_boundaries, min_distance, wall_margin)
     for j in range(num_round_tables):
         a.tables.append(table(Shape.circle, round_table_radius))
     for j in range(num_square_tables):
@@ -50,7 +57,69 @@ def main():
     
     print("Displaying final results")
     for oa in optimized_arrangements:
-        oa[0].display()
+        oa[0].display(oa[1])
+    
+    print("End Table Optimization")
+
+def optimize_number():
+    print("Begin Table Optimization")
+    
+    print("Loading configeration parameters")
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    min_distance=float(config['room']['minimum_separation'])
+    wall_margin=float(config['room']['wall_margin'])
+    x_boundaries=[0,int(config['room']['room_length'])]
+    y_boundaries=[0,int(config['room']['room_width'])]
+    
+    num_round_tables=int(config['tables']['number_of_round_tables'])
+    round_table_radius=float(config['tables']['round_table_radius'])
+    num_square_tables=int(config['tables']['number_of_square_tables'])
+    square_table_radius=float(config['tables']['square_table_side_length'])/2.0
+    
+    num_arrangements=int(config['algorithm']['number_of_arrangements'])
+    num_generations=int(config['algorithm']['number_of_generations'])
+    percent_parents_from_previous_generation=float(config['algorithm']['percent_parents_from_previous_generation'])
+    percent_new_arrangements_each_generation=float(config['algorithm']['percent_new_arrangements_each_generation'])
+    
+    num_round_tables=num_round_tables*10
+    num_square_tables=num_square_tables*10
+    final_error=(num_round_tables + num_square_tables) *2
+    epoch_counter=0
+    while final_error > 0.0 or (num_round_tables + num_square_tables) == 0 or epoch_counter > 10000:
+        print(" ** Epoch "+str(epoch_counter)+" ** ")
+        print(" * "+str(num_round_tables)+" round tables")
+        print(" * "+str(num_square_tables)+" square tables")
+        print("Initializing Tables for epoch: "+str(epoch_counter))
+        a = arrangement(x_boundaries, y_boundaries, min_distance, wall_margin)
+        for j in range(num_round_tables):
+            a.tables.append(table(Shape.circle, round_table_radius))
+        for j in range(num_square_tables):
+            a.tables.append(table(Shape.square, square_table_radius))
+        arrangements = []
+        for i in range(num_arrangements):
+            a.initializeTables(a.tables)
+            arrangements.append(a.copy())
+                    
+        print("Initializing Optimizer")
+        opt = optimizer(percent_parents_from_previous_generation, 
+                    percent_new_arrangements_each_generation)
+    
+        print("Running Optimizer")
+        optimized_arrangements = opt.optimize(arrangements, num_generations, False)
+        
+        final_error=optimized_arrangements[0][1]
+        print("Best result from epoch "+str(epoch_counter)+": "+str(final_error))
+        if final_error > 0.0:
+            if num_square_tables > 0:
+                num_square_tables -= 1
+            else:
+                num_square_tables = int(config['tables']['number_of_square_tables'])
+                num_round_tables -= 1
+        
+    print("Displaying final results")
+    for oa in optimized_arrangements:
+        oa[0].display(oa[1])
     
     print("End Table Optimization")
 
@@ -75,7 +144,7 @@ class table:
     
 class arrangement:
     
-    def __init__(self, min_distance=10, x_boundary=[0,100], y_boundary=[0,100], margin_distance=10):
+    def __init__(self, x_boundary=[0,100], y_boundary=[0,100], min_distance=10, margin_distance=10):
         self.rnd=Random()
         self.rnd.seed()
         self.tables=[]
@@ -107,37 +176,24 @@ class arrangement:
                     table.y = new_y
     
     def copy(self):
-        new_arrangement = arrangement(self.min_distance, self.x_boundary, self.y_boundary)
+        new_arrangement = arrangement(self.x_boundary, self.y_boundary, self.min_distance, self.margin_distance)
         for table in self.tables:
             new_arrangement.tables.append(table.copy())
         return new_arrangement
     
-    def display(self):
-        
-        round_x=[]
-        round_y=[]
-        round_r=[]
-        square_x=[]
-        square_y=[]
-        square_r=[]
-        scale_factor=5.2
-        for table in self.tables:
-            if table.shape is Shape.circle:
-                round_x.append(table.x)
-                round_y.append(table.y)
-                round_r.append(pow(table.radius,scale_factor))
-            elif table.shape is Shape.square:
-                square_x.append(table.x)
-                square_y.append(table.y)
-                square_r.append(pow(table.radius,scale_factor))
-        
-        plt.axes()        
+    def display(self, error=None):
+        plt.axes(xlim=self.x_boundary, ylim=self.y_boundary)
+        plt.grid(True, 'major', 'both')
         for table in self.tables:
             if table.shape is Shape.circle:
                 plt.gca().add_patch(self.__drawCircle(table.x, table.y, table.radius))
             elif table.shape is Shape.square:
                 plt.gca().add_patch(self.__drawSquare(table.x, table.y, table.radius))
-        plt.axis('scaled')
+                
+        if error is not None:
+            plt.title("Optimized Table Arrangement - Error = "+str(error))
+        else:
+            plt.title("Optimized Table Arrangement")
         plt.show()
     
     def __drawSquare(self, x, y, r):
@@ -164,10 +220,10 @@ class optimizer:
                 dx = i.x - j.x
                 dy = i.y - j.y
                 m = i.radius + arrangement.min_distance + j.radius
-                z = ( (pow(dx,2) + pow(dy,2)) / pow(m,2) )
+                z = (float(pow(dx,2) + pow(dy,2)) / float(pow(m,2))) -1.0
                 if z < 0:
-                    e = e + z
-            error += e
+                    e = e - z
+            error += (e -1)
         return error
         
     def testArrangements(self, arrangements):
@@ -195,27 +251,39 @@ class optimizer:
                 counter = 0
         return new_arrangements
             
-    def optimize(self, arrangements, num_generations):
-        print("Optimizing for",num_generations," generations.")
+    def optimize(self, arrangements, num_generations, print_generations=True):
+        if print_generations:
+            print("Optimizing for",num_generations," generations.")
         num_arrangements=len(arrangements)
         num_parents=int(num_arrangements*self.percent_parents)
         num_new=int(num_arrangements*self.percent_new)
         for i in range(num_generations):
-            print("Generation "+str(i)+":")
+            if print_generations:
+                print("Generation "+str(i)+":")
             results = self.testArrangements(arrangements)
             results.sort(key=lambda x:x[1])
             results = results[0:num_parents]
-            print(" - Top "+str(num_parents)+" arrangements' error results")
-            for r in results:
-                print("    "+str(r[1]))
+            if print_generations:
+                print(" - Top "+str(num_parents)+" arrangements' error results")
+                for r in results:
+                    print("    "+str(r[1]))
             parents=[r[0] for r in results]
-            print("Populating next generation...")
+            if print_generations:
+                print("Populating next generation...")
             arrangements = self.repopulateArrangements(parents, num_arrangements, num_new)
         return results
    
 if __name__ == "__main__":
-    main()
-        
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--optimize-number', action='store_true' ,help='Optimize the number and position of the tables within the room.')
+    parser.add_argument('--optimize-arrangement', action='store_true' ,help='Optimize only the position of the tables within the room.')
+    args = parser.parse_args()
+    if args.optimize_number:
+        optimize_number()
+    elif args.optimize_arrangement:
+        optimize_arrangement()
+    else:
+        parser.print_help()
             
             
         
